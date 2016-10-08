3c86e410b4d7f7f41ea9959087766f5d7ee8de1c
Paul MC Kim <kmc5500@naver.com>
2016-10-09 07:45:52 +0900

    allow domain

--- SagiJudge_webserver/index.js
+++ SagiJudge_webserver/index.js
@@ -11,6 +11,19 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.set('port', process.env.PORT || 3000);
 
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://medicop-minyoungjun.c9users.io/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/', function(req, res){
   res.render('index', {userName : "123"});
 });
