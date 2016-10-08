cb064448f702c75f595470dfac0705b2922b1064
Paul MC Kim <kmc5500@naver.com>
2016-10-09 07:48:21 +0900

    detach slash

--- SagiJudge_webserver/index.js
+++ SagiJudge_webserver/index.js
@@ -2,6 +2,20 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser').urlencoded({extended:true}); //form 평문전달
 
+
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://medicop-minyoungjun.c9users.io');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(bodyparser);
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
@@ -11,18 +25,6 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.set('port', process.env.PORT || 3000);
 
-// Add headers
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://medicop-minyoungjun.c9users.io/');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-});
 
 app.get('/', function(req, res){
   res.render('index', {userName : "123"});
