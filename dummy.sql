8cb4a7a056f11666b68d2f7ca099df44a09b6827
sungmin <smoh2044@gmail.com>
2016-10-09 06:50:45 +0900

    add procedure

--- dummy.sql
+++ dummy.sql
@@ -22,3 +22,23 @@ insert into rates (user_id, page_id, rate) values(6, 1, 1);
 insert into rates (user_id, page_id, rate) values(7, 1, 0);
 insert into rates (user_id, page_id, rate) values(8, 1, 1);
 insert into rates (user_id, page_id, rate) values(9, 1, 1);
+
+insert into rates (user_id, page_id, rate) values(1, 2, 0);
+insert into rates (user_id, page_id, rate) values(2, 2, 1);
+insert into rates (user_id, page_id, rate) values(3, 2, 0);
+insert into rates (user_id, page_id, rate) values(4, 2, 1);
+insert into rates (user_id, page_id, rate) values(5, 2, 0);
+insert into rates (user_id, page_id, rate) values(6, 2, 1);
+insert into rates (user_id, page_id, rate) values(7, 2, 0);
+insert into rates (user_id, page_id, rate) values(8, 2, 1);
+insert into rates (user_id, page_id, rate) values(9, 2, 1);
+
+insert into rates (user_id, page_id, rate) values(1, 3, 0);
+insert into rates (user_id, page_id, rate) values(2, 3, 1);
+insert into rates (user_id, page_id, rate) values(3, 3, 0);
+insert into rates (user_id, page_id, rate) values(4, 3, 1);
+insert into rates (user_id, page_id, rate) values(5, 3, 0);
+insert into rates (user_id, page_id, rate) values(6, 3, 1);
+insert into rates (user_id, page_id, rate) values(7, 3, 0);
+insert into rates (user_id, page_id, rate) values(8, 3, 1);
+insert into rates (user_id, page_id, rate) values(9, 3, 1);
--- procedure.sql
+++ procedure.sql
@@ -18,10 +18,10 @@ DROP PROCEDURE IF EXISTS `getBadOfPage`;
 CREATE PROCEDURE `getBadOfPage`(IN url VARCHAR(180))
 BEGIN
     SELECT count(*) AS bad
-        FROM rates AS r JOIN pages AS p
-        ON r.page_id = p._id
-        WHERE p.url = url
-        AND r.rate = 0;
+    FROM rates AS r JOIN pages AS p
+    ON r.page_id = p._id
+    WHERE p.url = url
+    AND r.rate = 0;
 END
 --
 
@@ -99,5 +99,60 @@ BEGIN
         GROUP BY u.opt
     ) AS b
     ON a.page_id = b.page_id AND a.opt = b.opt;
+END
+--
+
+DROP PROCEDURE IF EXISTS `getDangerPage`;
+--
+CREATE PROCEDURE `getDangerPage`()
+BEGIN
+    SELECT a.page_id, (a.ratio - b.ratio) as gap
+    FROM
+    (SELECT t.page_id as page_id, t.ratio as ratio FROM
+        (SELECT a.page_id as page_id,
+            a.opt as opt,
+            (b.good / (a.bad + b.good)) as ratio
+            FROM
+            (SELECT r.page_id as page_id, u.opt as opt, count(r.rate) AS bad
+                FROM rates AS r
+                JOIN users as u
+                ON r.user_id = u._id
+                WHERE r.rate = 0
+                GROUP BY r.page_id, u.opt
+            ) as a
+            JOIN
+            (SELECT r.page_id as page_id, u.opt as opt, count(r.rate) AS good
+                FROM rates AS r
+                JOIN users as u
+                ON r.user_id = u._id
+                WHERE r.rate = 1
+                GROUP BY r.page_id, u.opt
+            ) AS b
+            ON a.page_id = b.page_id AND a.opt = b.opt) AS t WHERE t.opt = 1) AS a
+    JOIN
+    (SELECT t.page_id as page_id, t.ratio as ratio
+        FROM (SELECT a.page_id as page_id,
+            a.opt as opt,
+            (b.good / (a.bad + b.good)) as ratio
+            FROM
+            (SELECT r.page_id as page_id, u.opt as opt, count(r.rate) AS bad
+                FROM rates AS r
+                JOIN users as u
+                ON r.user_id = u._id
+                WHERE r.rate = 0
+                GROUP BY r.page_id, u.opt
+            ) as a
+            JOIN
+            (SELECT r.page_id as page_id, u.opt as opt, count(r.rate) AS good
+                FROM rates AS r
+                JOIN users as u
+                ON r.user_id = u._id
+                WHERE r.rate = 1
+                GROUP BY r.page_id, u.opt
+            ) AS b
+            ON a.page_id = b.page_id AND a.opt = b.opt) AS t WHERE t.opt = 2) AS b
+    ON a.page_id = b.page_id
+    ORDER BY gap;
+
 END
 --
