1d364bbdc2261910811487c753af1b2fbffdfa8f
Paul MC Kim <kmc5500@naver.com>
2016-10-09 02:57:03 +0900

    add get_id function

--- SagiJudge_webserver/mc.js
+++ SagiJudge_webserver/mc.js
@@ -4,16 +4,33 @@
 var cheerio = require('cheerio');
 var request = require('request');
 
-var url = 'http://damoadamoa.tistory.com/121';
-request(url, function(error, response, html){
-	if (error) {throw error};
-
-	// console.log (html);
-
-	var $ = cheerio.load(html);
-
-	$('div.article p').each(function(){
-		console.log("설명" + $(this).text());
-	})
-
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host : '127.0.0.1',
+  database : 'lacidem',
+  user : 'guest',
+  password : '1234'
 });
+
+//
+// var url = 'http://damoadamoa.tistory.com/123';
+// request(url, function(error, response, html){
+// 	if (error) {throw error};
+//
+// 	// console.log (html);
+//
+// 	var $ = cheerio.load(html);
+// 	var result = "";
+// 	$('#entry div.article p').each(function(){
+// 		//console.log($(this).text());
+// 		result += $(this).text() + "\r\n";
+// 	});
+// 	var query = "CALL getIdByUrl("+ url + ", "+ JSON.stringify(result)  +")" ;
+// 	console.log("Query: "+ query);
+// 	pool.query(query, function (err, rows, fields){
+// 		console.log(fields);
+// 	});
+// });
+
+console.log(encodeURIComponent("http://damoadamoa.tistory.com/123"));
--- SagiJudge_webserver/routes/show.js
+++ SagiJudge_webserver/routes/show.js
@@ -1,28 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var util = require('util');
+var url = require('url');
+
+var cheerio = require('cheerio');
+var request = require('request');
 
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host : '127.0.0.1',
-  database : 'laos',
-  user : 'guest',
-  password : '1234'
+  database : 'lacidem',
+  user : 'lacidem',
+  password : 'lacidem',
 });
 
 router.route('/').get(function (req, res){
   res.render('test', {data:'default show'});
 });
 
-router.route('/get_url/:url').get(function(req, res){
-  var query = "";
-  pool.query(query, function (err, rows, fields){
+router.route('/get_id/:url').get(function(req, res){
+  request(req.params.url, function(error, response, html){
+  	if (error) {throw error};
 
-    // this is dummy data. // MC
-    console.log(JSON.stringify(rows));
-    res.type('text/plain');
-    res.send("13221");
+  	// console.log (html);
+
+  	var $ = cheerio.load(html);
+  	var result = "";
+  	$('#entry div.article p').each(function(){
+  		//console.log($(this).text());
+  		result += $(this).text() + "\r\n";
+  	});
+
+    var urlAddr = url.parse(req.params.url);
+    var urlStirng = urlAddr.host + urlAddr.pathname;
+    console.log(urlStirng);
+  	var query = "CALL getIdByUrl("+ urlAddr + ", "+ JSON.stringify(result)  +")" ;
+  	console.log("Query: "+ query);
+  	pool.query(query, function (err, rows, fields){
+  		console.log(fields);
+      res.type('text/plain');
+      res.send(JSON.stringify(rows));
+  	});
   });
 });
 
