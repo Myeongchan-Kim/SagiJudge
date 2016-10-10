30bfb1470d7bdb4b8532107358492fcdcaa2a9bc
Paul MC Kim <kmc5500@naver.com>
2016-10-10 14:32:47 +0900

    fix script

--- SagiJudge_webserver/index.js
+++ SagiJudge_webserver/index.js
@@ -5,14 +5,10 @@ var bodyparser = require('body-parser').urlencoded({extended:true}); //form ÌèâÎ
 
 // Add headers
 app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://medicop-minyoungjun.c9users.io');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
     next();
 });
 
